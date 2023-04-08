188a992619e944571df68ef42328c433236c882e
Marc Leerink <marc.leerink@planet.com>
2023-04-08 22:15:39 +0200

    merged calculate_area funcs

--- geo_helper/area.py
+++ geo_helper/area.py
@@ -3,39 +3,25 @@ from shapely.geometry.base import BaseGeometry
 from .reproject import reproject_geometry, reproject_geometry_local_utm
 
 
-def calculate_area_geometry(
+def calculate_area(
     geometry: BaseGeometry,
     source_epsg: int = 4326,
     target_epsg: int = 9822,
+    use_local_utm: bool = False,
     ) -> float:
     """
-    Calculate the area of a shapely geometry in square kilometers.
+    Calculate area of a geometry in square kilometers.
 
-    :param geometry: shapely geometry to calculate the area of
-    :param source_epsg: EPSG code of the source geometry.
-        Default is WGS84 (4326)
-    :param target_epsg: EPSG code of the target geometry.
-        Defaults is Lambert Conformal Conic (9822)
+    :param geometry: Geometry to calculate area for.
+    :param source_epsg: EPSG code of the geometry.
+    :param target_epsg: EPSG code of the target projection. Defaults is Lambert Conformal Conic (9822).
+        Only used if use_local_utm is False.
+    :param use_local_utm: If True, use local UTM projection instead of target_epsg.
+        Defaults to False.
 
-    :return: area in square kilometers
-    :raises TypeError: if the geometry type is not supported
     """
-    projected_geom = reproject_geometry(geometry, source_epsg, target_epsg)
-    return projected_geom.area / 1e6
-
-def calculate_area_geometry_local_utm(
-    geometry: BaseGeometry,
-    source_epsg: int = 4326,
-    ) -> float:
-    """
-    Calculate the area of a shapely geometry in square kilometers.
-
-    :param geometry: shapely geometry to calculate the area of
-    :param source_epsg: EPSG code of the source geometry.
-        Default is WGS84 (4326)
-
-    :return: area in square kilometers
-    :raises TypeError: if the geometry type is not supported
-    """
-    projected_geom = reproject_geometry_local_utm(geometry, source_epsg)
+    if use_local_utm:
+        projected_geom = reproject_geometry_local_utm(geometry, source_epsg)
+    else:
+        projected_geom = reproject_geometry(geometry, source_epsg, target_epsg)
     return projected_geom.area / 1e6
--- tests/test_area.py
+++ tests/test_area.py
@@ -2,8 +2,7 @@ from shapely.geometry import (GeometryCollection, LinearRing, LineString,
                               MultiLineString, MultiPoint, MultiPolygon, Point,
                               Polygon)
 
-from ..geo_helper.area import (calculate_area_geometry,
-                               calculate_area_geometry_local_utm)
+from ..geo_helper.area import calculate_area
 
 POINT = Point(8.0, 50.0)
 LINESTRING = LineString([(8.0, 50.0), (8.1, 50.1)])
@@ -15,26 +14,34 @@ MULTILINESTRING = MultiLineString([LINESTRING])
 GEOMETRYCOLLECTION = GeometryCollection(
     [POINT, LINESTRING, POLYGON, MULTIPOINT, MULTIPOLYGON, MULTILINESTRING])
 
-def test_calculate_area_geometry_point():
-    assert calculate_area_geometry(POINT) == 0.0
+def test_calculate_area_point():
+    assert calculate_area(POINT) == 0.0
+    assert calculate_area(POINT, use_local_utm=True) == 0.0
 
-def test_calculate_area_geometry_linestring():
-    assert calculate_area_geometry(LINESTRING) == 0.0
+def test_calculate_area_linestring():
+    assert calculate_area(LINESTRING) == 0.0
+    assert calculate_area(LINESTRING, use_local_utm=True) == 0.0
 
-def test_calculate_area_geometry_linearring():
-    assert calculate_area_geometry(LINEARRING) == 0.0
+def test_calculate_area_linearring():
+    assert calculate_area(LINEARRING) == 0.0
+    assert calculate_area(LINEARRING, use_local_utm=True) == 0.0
 
-def test_calculate_area_geometry_polygon():
-    assert calculate_area_geometry(POLYGON) == 39.873604788232235
+def test_calculate_area_polygon():
+    assert calculate_area(POLYGON) == 39.873604788232235
+    assert calculate_area(POLYGON, use_local_utm=True) == 39.846002633404
 
-def test_calculate_area_geometry_multipoint():
-    assert calculate_area_geometry(MULTIPOINT) == 0.0
+def test_calculate_area_multipoint():
+    assert calculate_area(MULTIPOINT) == 0.0
+    assert calculate_area(MULTIPOINT, use_local_utm=True) == 0.0
 
-def test_calculate_area_geometry_multipolygon():
-    assert calculate_area_geometry(MULTIPOLYGON) == 39.873604788232235
+def test_calculate_area_multipolygon():
+    assert calculate_area(MULTIPOLYGON) == 39.873604788232235
+    assert calculate_area(MULTIPOLYGON, use_local_utm=True) == 39.846002633404
 
-def test_calculate_area_geometry_multilinestring():
-    assert calculate_area_geometry(MULTILINESTRING) == 0.0
+def test_calculate_area_multilinestring():
+    assert calculate_area(MULTILINESTRING) == 0.0
+    assert calculate_area(MULTILINESTRING, use_local_utm=True) == 0.0
 
-def test_calculate_area_geometry_geometrycollection():
-    assert calculate_area_geometry(GEOMETRYCOLLECTION) == 79.74720957646447
+def test_calculate_area_geometrycollection():
+    assert calculate_area(GEOMETRYCOLLECTION) == 79.74720957646447
+    assert calculate_area(GEOMETRYCOLLECTION, use_local_utm=True) == 79.692005266808
